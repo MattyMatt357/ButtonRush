76c56961acd51c5dec2a76a5bb6cf6477a86b291
MattyMatt357 <mc21517@my.acm.ac.uk>
2024-08-04 19:32:03 +0100

    Deleted commented out code

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -37,35 +37,20 @@ public class PlayerMovement : MonoBehaviour, PlayerInputActions.IPlayerActions
     // Update is called once per frame
     void Update()
     {
-        // playerDirection = inputActions.Player.Move.ReadValue<Vector3>();
         transform.rotation = Quaternion.Euler(0, camera.transform.eulerAngles.y,0);
-
-        
-
         Vector3 playerMovement = new Vector3(playerDirection.x, 0.0f, playerDirection.z).normalized;
         float forward = 1.5f;
-
         playerMovement = (camera.forward * playerMovement.z * forward) + camera.right * playerMovement.x;
         rigidbody.AddForce(playerMovement * playerSpeed * 1.5f * Time.deltaTime, ForceMode.Acceleration);
-
     }
-
-    
-
+   
     public void OnMove(InputAction.CallbackContext context)
-    {
-        //Debug.Log(context.ReadValue<Vector3>());
-        playerDirection = context.ReadValue<Vector3>();
-      //  playerDirection = camera.forward + context.ReadValue<Vector3>();
-       // playerDirection = playerDirection + camera.right;
-      //  playerDirection.Normalize();
-
-       // rigidbody.AddForce(new Vector3(playerDirection.x, 0, playerDirection.z) * playerSpeed, ForceMode.Force);
+    {      
+        playerDirection = context.ReadValue<Vector3>();     
     }
 
     public void OnLook(InputAction.CallbackContext context)
-    {
-       // Debug.Log(context.ReadValue<Vector2>());
+    {      
         cameraRotation = context.ReadValue<Vector2>();
     }   
 
